0b625fdb50c8e79246f980aef65046162a712918
Roman <dragon3h@gmx.com>
2019-04-06 07:06:21 -0400

    initial migration

---
+++ DotNetCleanArch.API/CleanApp.Persistance/CleanAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CleanApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanApp.Persistence
+{
+    public class CleanAppDbContext : DbContext
+    {
+        public CleanAppDbContext(DbContextOptions<CleanAppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<BuyOrder> BuyOrders { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Mascot> Mascots { get; set; }
+        public DbSet<RentOrder> RentOrders{ get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -13,4 +13,8 @@
 3. BE:
    a) in Domain project create Models
    b) in Infrastructure -> Persistance project add EntityFramework and neccessary tools
-   c) in Infrastructure -> Persistance project create DbContext and configure it in Startup.cs
+   c) in Infrastructure -> Persistance project create DbContext
+   d) configure SQL Server Provider in Presentation -> WebAPI in file Startup.cs
+   e) add connection string in Presentation -> WebAPI appsettings.json
+   f) add DbSet(s) to CleanAppDbContext
+   g) create initial migration
