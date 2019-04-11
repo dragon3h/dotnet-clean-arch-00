c742f40c03e41a20ae7c5da0cfb299033e829447
Roman <dragon3h@gmx.com>
2019-04-10 21:07:07 -0400

    reinstall Vue project and add Vuetify

--- dotnet-cleanarch-spa/src/main.js
+++ dotnet-cleanarch-spa/src/main.js
@@ -4,9 +4,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-import Vuetify from "vuetify";
-
-Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
